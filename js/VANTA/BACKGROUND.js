3f637594e85cbe904c9e2199a0054ef3180e446f
LAMONA.DEV <aymanmohamed.programmer@gmail.com>
2025-07-06 23:31:02 +0300

    finish music player app

---
+++ js/VANTA/BACKGROUND.js
@@ -0,0 +1,12 @@
+VANTA.TRUNK({
+    el: "body",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.0,
+    minWidth: 200.0,
+    scale: 1.0,
+    scaleMobile: 1.0,
+    spacing: 9.5,
+    chaos: 5.5,
+});
